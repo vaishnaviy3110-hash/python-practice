c287519f1156021e3fd187cd54e717563d753a34
Vaishnavi Yadav <vaishnaviy3110@gmail.com>
2026-08-06 09:40:01 +0530

    Add simple calculator functionality in Python

--- basics/calculator.py
+++ basics/calculator.py
@@ -1,1 +1,19 @@
+print("===== Simple Calculator =====")
 
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    print("Result:", num1 + num2)
+elif operator == "-":
+    print("Result:", num1 - num2)
+elif operator == "*":
+    print("Result:", num1 * num2)
+elif operator == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error! Division by zero is not allowed.")
+else:
+    print("Invalid operator!")
