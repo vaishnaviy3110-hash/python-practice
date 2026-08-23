5a97a8dd050be2dd96d73f802a5b39a79afab3e2
Vaishnavi Yadav <vaishnaviy3110@gmail.com>
2026-08-23 22:05:42 +0530

    Add Reverse.py to reverse a number

---
+++ loops/Reverse.py
@@ -0,0 +1,12 @@
+# Program to reverse a number
+
+num = int(input("Enter a number: "))
+
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+print("Reverse of the number is:", reverse)
